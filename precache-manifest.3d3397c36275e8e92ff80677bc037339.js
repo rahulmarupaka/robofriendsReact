7a38c9a5eb9a4826979bdad97e7efb50b9d86522
Rahul Marupaka <rahulmarupaka@gmail.com>
2018-11-01 11:16:59 -0500

    Robofriends website using ReactJS

---
+++ precache-manifest.3d3397c36275e8e92ff80677bc037339.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "03680651b8f6e4fa4b05",
+    "url": "/static/js/main.03680651.chunk.js"
+  },
+  {
+    "revision": "4df0fd8969feba270f22",
+    "url": "/static/js/1.4df0fd89.chunk.js"
+  },
+  {
+    "revision": "03680651b8f6e4fa4b05",
+    "url": "/static/css/main.3b670b5d.chunk.css"
+  },
+  {
+    "revision": "4df0fd8969feba270f22",
+    "url": "/static/css/1.0669d4d6.chunk.css"
+  },
+  {
+    "revision": "de095204598b9de135ec6b2d0d25c805",
+    "url": "/index.html"
+  }
+];
